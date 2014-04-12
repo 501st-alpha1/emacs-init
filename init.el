af5a1482dd6619e70be5122225a6c78503d7fe97
Scott Weldon <scott@scott-weldon.com>
2014-04-12 14:41:52 -0700

    Auto-save, but in the home directory

--- init.el
+++ init.el
@@ -70,3 +70,11 @@
 (display-battery-mode 1)
 (setq battery-mode-line-format " [%b%p%%] ")
 
+;Auto Backup Files
+(setq backup-directory-alist '(("." . "~/.saves"))
+      backup-by-copying t
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
