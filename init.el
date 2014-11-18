36fcadcf5931a102c0ea443bbf4737cdb36a4731
Scott Weldon <scott@scott-weldon.com>
2014-11-18 13:32:46 -0800

    Disabling uniquify until I can clean up names
    I need a way to differentiate between e.g. two different dired buffers
    of the same folder name, but I don't need everything else to always show
    the full path.

--- init.el
+++ init.el
@@ -454,8 +454,8 @@
            nl (if (= (user-uid) 0) "# " "$ ")))))
 
 ;; Uniquify
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets
-      uniquify-min-dir-content 999)
+;(setq uniquify-buffer-name-style 'post-forward-angle-brackets
+;      uniquify-min-dir-content 999)
 
 ;;----------------------------------------------------------------------------;;
 ;;                           Default-Frame-Alist                              ;;
