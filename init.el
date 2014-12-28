0964da9aee8c2c83b4bd991b2ebd682ac1a9cf9b
Scott Weldon <scott@scott-weldon.com>
2014-12-27 21:50:27 -0800

    Make waiting tasks yellow

--- init.el
+++ init.el
@@ -480,6 +480,7 @@ If OTHERS is true, skip all entries that do not correspond to TAG."
 (setq org-use-fast-todo-selection t
       org-agenda-skip-deadline-prewarning-if-scheduled t
       org-log-into-drawer t
+      org-todo-keyword-faces '(("WAITING" . "yellow"))
       org-stuck-projects '("TODO={.+}/-DONE" nil nil "SCHEDULED:\\|DEADLINE:"))
 (add-hook 'org-after-todo-statistics-hook 'my-org-summary-todo)
 
