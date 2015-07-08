c41c4ce33617e645e9d5a5f949f009c6deb4b741
Scott Weldon <scottw@scvdata.com>
2015-07-08 12:55:41 -0700

    Add my-fix-curly-braces to my-format-buffer

--- init.el
+++ init.el
@@ -229,6 +229,7 @@
 (defun my-format-buffer()
   (interactive)
   (untabify (point-min) (point-max))
+  (my-fix-curly-braces (point-min) (point-max))
   (indent-region (point-min) (point-max)))
 
 (defun my-full-magit-log(directory short-name)
