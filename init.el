2df9a3a190c041bc5a4a28437b10132931e30e37
Scott Weldon <scottw@scvdata.com>
2015-07-08 14:51:37 -0700

    Add argument spacing to format-buffer

--- init.el
+++ init.el
@@ -258,6 +258,7 @@
   (untabify (point-min) (point-max))
   (my-fix-curly-braces (point-min) (point-max))
   (my-fix-spacing (point-min) (point-max))
+  (my-fix-argument-spacing (point-min) (point-max))
   (indent-region (point-min) (point-max)))
 
 (defun my-full-magit-log(directory short-name)
