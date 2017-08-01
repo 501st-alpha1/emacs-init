73819201c94d86efb8e633f26ffa7c7c2682d364
Scott Weldon <open-source@scott-weldon.com>
2017-08-01 10:10:40 -0700

    Default org-agenda-span to day

--- init.el
+++ init.el
@@ -740,6 +740,7 @@ To modify this variable, you can use the customize interface, or do e.g.:
       org-agenda-sorting-strategy '((agenda time-up priority-down tag-up))
       org-log-into-drawer t
       org-log-done 'time
+      org-agenda-span 'day
       org-extend-today-until 4
       org-expiry-inactive-timestamps t
       org-clock-persist t
